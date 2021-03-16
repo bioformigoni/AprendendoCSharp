9079e7566bd82ace2d44d256e12665bbe94e03a6
bioformigoni <bioformigoni@gmail.com>
2021-03-15 21:11:10 -0300

    Caracteres e textos

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caractéres e Textos");
+            Console.WriteLine("");
+
+            // apenas 1 caracter 16bits
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            // numero inteiro tem 32bits, por isso é necessária a conversão
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            // É possivel fazer manipulações numéricas com os caractéres
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            // A transformação de numero para caractere segue a Tabela ASC (ex: 61 é "=")
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura cursos de tecnologia";
+            Console.WriteLine(titulo);
+
+            Console.WriteLine("");
+
+            titulo = "Alura cursos de tecnologia " + 2020;
+            Console.WriteLine(titulo);
+
+            string cursosProgramacao = " - .NET" +
+                " - Java" +
+                " - JavaScript";
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("");
+
+            Console.WriteLine(titulo);
+            cursosProgramacao = @" - .NET   
+                - Java 
+                - JavaScript";
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("");
+
+            Console.WriteLine(titulo);
+            cursosProgramacao = 
+@" - .NET   
+ - Java 
+ - JavaScript";
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("");
+            Console.WriteLine("A execução acabou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6");
+            Console.WriteLine("");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.WriteLine("");
+            Console.WriteLine("A execução acabou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}

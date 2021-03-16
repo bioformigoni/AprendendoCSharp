f56166efb0c76939a94311280b5b1c4e671dcaa3
bioformigoni <bioformigoni@gmail.com>
2021-03-16 09:10:45 -0300

    8 Condicionais 2

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8 - Condicionais 2");
+            Console.WriteLine("");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+            if (idadeJoao >= 18 || quantidadePessoas >= 2)
+            {
+                Console.WriteLine("João pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar.");
+            }
+
+            if (idadeJoao >= 18 && quantidadePessoas >= 2)
+            {
+                Console.WriteLine("João pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar.");
+            }
+
+            idadeJoao = 18;
+            bool acompanhando = quantidadePessoas >= 2;
+            if (idadeJoao >= 18 && acompanhando)
+            {
+                Console.WriteLine("João pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar.");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("A execução acabou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
+

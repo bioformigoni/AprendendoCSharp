fa40d09055e1deb39605f85245205a1d22d76f36
bioformigoni <bioformigoni@gmail.com>
2021-03-20 17:36:15 -0300

    11 Calcula Poupanca 2

---
+++ P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11 - Calcula poupança 2");
+            Console.WriteLine("");
+
+            double valorInvestido = 1000;
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+
+                if (contadorMes == 1)
+                {
+                    Console.WriteLine("Após " + contadorMes + " mês, você terá R$" + valorInvestido);
+                }
+                else
+                {
+                    Console.WriteLine("Após " + contadorMes + " mêses, você terá R$" + valorInvestido);
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("A execução acabou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}

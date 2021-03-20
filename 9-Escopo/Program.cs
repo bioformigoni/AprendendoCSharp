449da21aba7196317f6ae35eeeb0113a66c1682f
bioformigoni <bioformigoni@gmail.com>
2021-03-20 17:19:35 -0300

    10 Calcula poupanca

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+            Console.WriteLine("");
+
+            int idadeJoao = 18;
+            bool acompanhando = true;
+            string mensagemAdicional;
+
+            if (acompanhando == true)
+            {
+                mensagemAdicional = "João está acompanhado";
+                Console.WriteLine(mensagemAdicional);
+            }
+            else 
+                mensagemAdicional = "João não está acompanhado";
+            
+
+            if (idadeJoao >= 18 || acompanhando)
+            {
+                Console.WriteLine("João pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else Console.WriteLine("João não pode entrar.");
+            
+
+            Console.WriteLine("");
+            Console.WriteLine("A execução acabou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula poupança");
+            Console.WriteLine("");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                // 0.36% = 0.0036
+                valorInvestido += valorInvestido * 0.0036;
+
+                if (contadorMes == 1)
+                {
+                    Console.WriteLine("Após " + contadorMes + " mês, você terá R$" + valorInvestido);
+                }
+                else
+                {
+                    Console.WriteLine("Após " + contadorMes + " mêses, você terá R$" + valorInvestido);
+                }
+
+                contadorMes ++;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("A execução acabou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}

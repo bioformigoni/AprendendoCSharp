178e8958746192681067671f9fd8a5abfc3e5045
bioformigoni <bioformigoni@gmail.com>
2021-03-22 09:02:22 -0300

    12 Calculo de investimento a longo prazo

---
+++ P12CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 12 - Calcula investimento a longo prazo");
+            Console.WriteLine("");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
+
+            Console.WriteLine("");
+            Console.WriteLine("A execução acabou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
